60dc3e854905bf41e7e481c37368bf8cd3179f67
Bill <william.hine2019@gmail.com>
2020-05-26 12:09:58 -0400

    init

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProj;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
