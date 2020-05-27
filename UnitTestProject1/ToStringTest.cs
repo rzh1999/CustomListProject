36285e30590502728970888173f9a0b2ed861aab
Bill <william.hine2019@gmail.com>
2020-05-27 19:37:12 -0400

    added Tostring_OneString_StringEquals() Tostring_FourStrings_AllStringsEqual() Tostring_NullString_StringEqualsNull()

--- UnitTestProject1/ToStringTest.cs
+++ UnitTestProject1/ToStringTest.cs
@@ -12,9 +12,68 @@ namespace UnitTestProject1
     public class ToStringTest
     {
         [TestMethod]
-        public void Tostring_StringOfCustomList_StringEquals()
+        public void Tostring_OneString_StringEquals()
         {
-            CustomList<int> customList = new CustomList<int>();
+            CustomList<string> customList = new CustomList<string>();
+
+            customList.Add("Bill");
+
+            string expected = "Bill";
+            string actual;
+
+            actual = customList.ToString();
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void Tostring_FourStrings_AllStringsEqual()
+        {
+            CustomList<string> customList = new CustomList<string>();
+
+            customList.Add("Bill");
+            customList.Add("needs");
+            customList.Add("more");
+            customList.Add("work");
+
+            string expectedBill = "Bill";
+            string actual1;
+
+            string expectedNeeds = "needs";
+            string actual2;
+
+            string expectedMore = "more";
+            string actual3;
+
+            string expectedWork = "work";
+            string actual4;
+
+            actual1 = customList.ToString();
+            actual2 = customList.ToString();
+            actual3 = customList.ToString();
+            actual4 = customList.ToString();
+
+            Assert.AreEqual(actual1, expectedBill);
+            Assert.AreEqual(actual2, expectedNeeds);
+            Assert.AreEqual(actual3, expectedMore);
+            Assert.AreEqual(actual4, expectedWork);
+        }
+
+        [TestMethod]
+        public void Tostring_NullString_StringEqualsNull()
+        {
+            CustomList<string> customList = new CustomList<string>();
+
+            string nullTest = null;
+
+            customList.Add(nullTest);
+
+            string expected = null;
+            string actual;
+
+            actual = customList.ToString();
+
+            Assert.AreEqual(actual, expected);
         }
     }
 }
