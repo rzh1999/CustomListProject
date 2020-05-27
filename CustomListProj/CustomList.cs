ea59808ebc2452760d9aa49cc6808bf6acb8fcb5
Bill <william.hine2019@gmail.com>
2020-05-27 10:42:28 -0400

    Completed Add()

--- CustomListProj/CustomList.cs
+++ CustomListProj/CustomList.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +12,95 @@ namespace CustomListProj
     {
        
         private T[] items;
-        private int count = 0;
+        private int count;
         private int capacity;
-
+       
         public CustomList()
         {
-            items = new T[4];
+            count = 0;
+            capacity = 4;
+            items = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                //if (index < 0 && index >= items.Length)
+                    //throw new IndexOutOfRangeException("Index out of range");
+
+                return items[index];
+            }
+
+            set
+            {
+                //if (index < 0 || index >= items.Length)
+                //    throw new IndexOutOfRangeException("Index out of range");
+
+                items[index] = value;
+            }
         }
         public void Add(T item)
         {
+            T[] temp = new T[capacity];
+            
+            //inserting into the array
+            if (count == 0)
+            {
+                items[count] = item;
+                count++;
+            }
+            else if (count > 0 && count < 4)
+            {
+                items[count] = item;
+                count++;
+            }
+            else if (count >= 4 && items.Length >=4)
+            {
 
+               
+
+                //foreach (T element in items)
+                //{
+                //    temp[index] = element;
+                //   //if (items.Length > 5)
+                //   // {
+                //   //     temp[index] = element;
+                //   // }
+                //    index++;
+
+                //}
+                for (int i = 0; i < count; i++)
+                {
+                    temp[i] = items[i];
+                    
+                    
+                }
+                
+                items = new T[capacity *= 2];
+               
+                for (int i = 0; i < count; i++)
+                {
+                    items[i] = temp[i];
+                   
+                }
+                items[count] = item;
+                //foreach(T elements in temp)
+                //{
+                //    items[copyIndex] = elements;
+                //    copyIndex++;
+                //}
+                // items[copyIndex] = item;
+                //copyIndex++;
+                count++;
+            }
+            //if count and capacity are equal
+            //then increase capacity
+            //copy values over to new array
 
         }
 
-       
+
 
     }
 }
--- CustomListProj/Program.cs
+++ CustomListProj/Program.cs
@@ -10,6 +10,48 @@ namespace CustomListProj
     {
         static void Main(string[] args)
         {
+            CustomList<int> customList = new CustomList<int>();
+            //customList[0] = 2;
+            //customList[1] = 3;
+
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            customList.Add(5);
+            customList.Add(6);
+            customList.Add(7);
+            customList.Add(8);
+            customList.Add(9);
+            customList.Add(10);
+            customList.Add(11);
+            customList.Add(12);
+            customList.Add(13);
+            customList.Add(14);
+            customList.Add(15);
+            customList.Add(16);
+            customList.Add(17);
+
+            Console.WriteLine(customList[0]);
+            Console.WriteLine(customList[1]);
+            Console.WriteLine(customList[2]);
+            Console.WriteLine(customList[3]);
+            Console.WriteLine(customList[4]);
+            Console.WriteLine(customList[5]);
+            Console.WriteLine(customList[6]);
+            Console.WriteLine(customList[7]);
+            Console.WriteLine(customList[8]);
+            Console.WriteLine(customList[9]);
+            Console.WriteLine(customList[10]);
+            Console.WriteLine(customList[11]);
+            Console.WriteLine(customList[12]);
+            Console.WriteLine(customList[13]);
+            Console.WriteLine(customList[14]);
+            Console.WriteLine(customList[15]);
+            Console.WriteLine(customList[16]);
+
+            Console.ReadLine();
+
         }
     }
 }
